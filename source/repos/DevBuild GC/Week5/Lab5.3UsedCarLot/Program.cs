9b1dc72cc79451f785bd10d37b31ab448267fdb2
Mark Ellis <markellis@quickenloans.com>
2020-02-17 09:18:43 -0500

    submit

---
+++ source/repos/DevBuild GC/Week5/Lab5.3UsedCarLot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab5._3UsedCarLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
